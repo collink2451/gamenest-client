f0632b5378191493e173903277e43026def55343
Collin Koldoff <collinkoldoff@gmail.com>
2024-04-16 23:58:55 -0500

    Added labels to battleship

---
+++ src/components/Battleship/BoardLabelRow.jsx
@@ -0,0 +1,19 @@
+const BoardLabelRow = () => {
+
+    function drawRow() {
+        const letters = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        return letters.map((letter) => {
+            return (
+                <div className="col square">{letter}</div>
+            );
+        });
+    }
+
+    return (
+        <div className="row row-cols-11">
+            {drawRow()}
+        </div>
+    );
+}
+
+export default BoardLabelRow;
--- src/components/Battleship/BoardRow.jsx
+++ src/components/Battleship/BoardRow.jsx
@@ -5,13 +5,14 @@ const BoardRow = ({ row, rowIndex, onClick, onMouseOver, onMouseOut }) => {
     function drawRow() {
         return row.map((cell, cellIndex) => {
             return (
-                <Square key={cellIndex} state={cell} onClick={() => onClick(rowIndex, cellIndex)} />
+                <Square key={cellIndex} state={cell} onClick={() => onClick(rowIndex, cellIndex)} onMouseOver={() => onMouseOver(rowIndex, cellIndex)} />
             );
         });
     }
 
     return (
         <div className="row row-cols-11">
+            <div className="col square">{rowIndex + 1}</div>
             {drawRow()}
         </div>
     );
