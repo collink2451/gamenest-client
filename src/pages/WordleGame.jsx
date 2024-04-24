539416fa9b7870d0f704ce55c7ad5f88d06d7647
juliedosher <juliegdosher@lewisu.edu>
2024-04-24 10:02:40 -0500

    added api to get random word

--- src/pages/WordleGame.jsx
+++ src/pages/WordleGame.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
+const api = 'https://random-word-api.herokuapp.com/word?length=5'
+
 const WordleGame = () => {
+    const [word, setWord] = useState([])
+
+    function getWord() {
+        fetch(api)
+            .then(response => response.json())
+            .then(data => setWord(data))
+            .catch(err => console.error("Error fetching word: ", err))
+        console.log(word)
+    }
+   
     
     return (
         <Container>
             <Row>
                 <Col>
                     <h1>Wordle Game</h1>
+                    <button onClick={getWord}>Start Game</button>
                 </Col>
             </Row>
         </Container>
