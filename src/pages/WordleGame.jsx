d62f063ffef5adae05dc677a58a8d57aee34abd7
juliedosher <juliegdosher@lewisu.edu>
2024-04-26 01:27:05 -0500

    adjusted function call

--- src/pages/WordleGame.jsx
+++ src/pages/WordleGame.jsx
@@ -5,19 +5,17 @@ const api = 'https://random-word-api.herokuapp.com/word?length=5'
 
 const WordleGame = () => {
     const [word, setWord] = useState([])
+    
     async function getWord() {
         fetch(api)
             .then(response => response.json())
             .then(data => setWord(data[0]))
             .catch(err => console.error("Error fetching word: ", err))
-        if (Array.isArray(word)) {
-           console.log("oops")
-        }
         console.log(word)
     }
 
     useEffect(() => {
-        getWord();
+        getWord()
      }, []);
     
     
